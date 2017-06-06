7d01d5d9c9ea815754ea2675649b0c420011110b
Flynn <flynnigan.tan@gmail.com>
2017-06-05 21:23:42 -0500

    updated css and html; fixed js map error, added yt

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -21,197 +21,250 @@ function initMap(){
 
 $(document).ready(function(){
 
-function twitter(twitterURL){
-	// console.log(twitterURL);
+	$('#bottom').hide();
 
-	var splitArr = twitterURL.split("https://twitter.com/");
-	var twitterHandle = splitArr[1];
-	// console.log(twitterHandle);
+	// TWITTER FUNCTION 
+	function twitter(twitterURL){
+		console.log("twitterURL: " + twitterURL);
 
-	twttr.widgets.createTimeline({
-    	sourceType: 'profile',
-    	screenName: twitterHandle
-  		},
-  		document.getElementById('twitcontainer'),
-  		{
-  			theme: "dark",
-  		}
-	);
-}
+		var splitArr = twitterURL.split("https://twitter.com/");
+		var twitterHandle = splitArr[1];
+		console.log("twitterHandle: " + twitterHandle);
 
-// We can change eventsResponse to another name as to not confuse it with the other
-function concertInformation(eventsResponse){			
-	var concertName;
-	var city;
-	var state;
-	var venueName;
-	var startDate;
-	var ticketLink;
-	var time;
-	var formatTime;
-
-	for (var i = 0; i < 10 && i < eventsResponse._embedded.events.length; i++) {
-
-		// grab concert name from each event
-		concertName = eventsResponse._embedded.events[i].name;
-		// console.log("concert " + (i+1) + " name: " + concertName);
-
-		// grab city
-		city = eventsResponse._embedded.events[i]._embedded.venues[0].city.name;
-		// console.log("city: " + city);
-
-		// grab state
-		state = eventsResponse._embedded.events[i]._embedded.venues[0].state.stateCode;
-		// console.log("state: " + state);
-
-		// grab venue name from each event
-		venueName = eventsResponse._embedded.events[i]._embedded.venues[0].name;
-		// console.log("venue " + (i+1) + " name: " + venueName);
-		
-		// grab concert time
-		// validate that the time has been set first
-		if (eventsResponse._embedded.events[i].dates.start.timeTBA == false && eventsResponse._embedded.events[i].dates.start.noSpecificTime == false) {
-			time = eventsResponse._embedded.events[i].dates.start.localTime;
-			formatTime = moment(time, "HH:mm:ss").format("h:mm A");
+		try {
+			twttr.widgets.createTimeline({
+		    	sourceType: 'profile',
+		    	screenName: twitterHandle
+	  		},
+	  		document.getElementById('twitcontainer'),
+	  		{
+	  			theme: "dark",
+	  		});
 		}
-		else if (eventsResponse._embedded.events[i].dates.start.timeTBA == false && eventsResponse._embedded.events[i].dates.start.noSpecificTime == true) {
-			formatTime = "This event has no specific time.";
+		catch(e) {
+			$('#twitter-display').html("<h4>Sorry, this Twitter account is unavailable.</h4>");
 		}
-		else {
-			formatTime = "This event's time hasn't been set yet!";
-		}
-
-		// grab concert start date
-		// validate that the date has been set first
-		if (eventsResponse._embedded.events[i].dates.start.dateTBA == false && eventsResponse._embedded.events[i].dates.start.dateTBD == false){
-			startDate = eventsResponse._embedded.events[i].dates.start.localDate;
-			// console.log("date: " + startDate);
-		}
-		else {
-			startDate = "The date hasn't been set yet!";
-		}
-
-		// grab ticket link
-		ticketLink = eventsResponse._embedded.events[i].url;
-		// console.log("Tickets for Concert " + (i+1) + ": " + ticketLink);
-		
-		$('#concerts-display').append(
-			"<div class='panel panel-info'>" +
-			"<div class='panel-heading'>" +
-			"<h3 class='panel-title'>" +
-			concertName + " <small>" + city + ", "  + state + 
-			"</small>" + "</h3></div>" +
-			"<div class='panel-body'>" +
-			venueName + "<br/>" + formatTime + "<br/>" +
-			"<a href='" + ticketLink + "'>Purchase Tickets Here</a>" +
-			"</div></div>"
-		);
 	}
-}
 
-function ticketmasterRequest(artist){
-	// console.log(artist);
+	// YOUTUBE FUNCTION
 
-	// This will be the URL to obtain the the unique attractionId for the artist 
-	// We need this so we can find the upcoming events for the exact artist
-	var attractionsURL = "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=JuO6exfNpASFTwptmBPvKKqdGYrP7S96&keyword=" + artist;  
+	function youtube(youtubeURL){
+		console.log("youtubeURL:" + youtubeURL);
 
-	// AJAX REQUEST #1: Obtaining information about artist
-	$.ajax({
-		url: attractionsURL,
-		method: "GET"
-	})
-	.done(function(artistResponse){
-		console.log(artistResponse);
+		var splitArray = youtubeURL.split("https://www.youtube.com/user/");
+		var youtubeUser = splitArray[1];
+		console.log("youtubeUser: " + youtubeUser);
 
-		var checkArtist = artistResponse.page.totalElements;
-
-		// If input error is incorrect, alert the user. Otherwise, continue...
-		if (checkArtist === 0) {
-			// This is the message for the user, uncomment when .search-error is added to html
-			// $(".search-error").html("*Sorry, try again");
-			// console.log("Sorry, try again");
+		try {
+			$('iframe').attr("src", "https://www.youtube.com/embed?enablejsapi=1&controls=2&showinfo=1&rel=0&listType=user_uploads&list=" + youtubeUser);
 		}
-		else {
-			var artistId = artistResponse._embedded.attractions[0].id; 
 
-			// Grabbing the artist's twitter url and calling the twitter function
-			var twitterURL = artistResponse._embedded.attractions[0].externalLinks.twitter[0].url;
-			// console.log(twitterURL);
-			twitter(twitterURL);
+		catch(e) {
+			$('#youtube-display').html("<h4>Sorry, this Youtube Channel is unavailable.</h4>");
+		}
+	}
 
-			var eventsURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=JuO6exfNpASFTwptmBPvKKqdGYrP7S96&attractionId="+ artistId +  "&countryCode=" + countryCode + "&sort=" + dateOrder + "&endDateTime=" + sixMonthsLimit + "Z";
-				
-			// AJAX REQUEST #2: Obtaining information about artist's upcoming events
-			$.ajax({
-				url: eventsURL,
-				method: "GET"
-			}) 
-			.done(function(eventsResponse){
-				console.log(eventsResponse);
+	// CONCERT INFORMATION
+	// We can change eventsResponse to another name as to not confuse it with the other
+	function concertInformation(eventsResponse){			
+		var concertName;
+		var city;
+		var state;
+		var venueName;
+		var startDate;
+		var ticketLink;
+		var time;
+		var formatTime;
 
-				var upcomingEvents = eventsResponse.page.totalElements;
+		for (var i = 0; i < 10 && i < eventsResponse._embedded.events.length; i++) {
 
-				// If there are no concerts, alert the user. Otherwise, continue...
-				if (upcomingEvents === 0) {
-					// Display this message to the user if there are no shows
-					$("#concerts-display").html("It seems there are no upcoming shows");
+			// grab concert name from each event
+			concertName = eventsResponse._embedded.events[i].name;
+			// console.log("concert " + (i+1) + " name: " + concertName);
 
-					// console.log("It seems there are no upcoming shows");
-				}
-				else {
-					// console.log(eventsResponse);
+			// grab city
+			city = eventsResponse._embedded.events[i]._embedded.venues[0].city.name;
+			// console.log("city: " + city);
 
-					concertInformation(eventsResponse);
+			// grab state
+			state = eventsResponse._embedded.events[i]._embedded.venues[0].state.stateCode;
+			// console.log("state: " + state);
 
-					// Google Maps Markers
-					var numEvents = eventsResponse._embedded.events;
-					for (var i = 0; i < numEvents.length ; i++) {
-						// Sometimes there is not lat/long for venues, so if-else statement
-						if(eventsResponse._embedded.events[i]._embedded.venues[0].location === undefined){
-							// We need to change this console.log to some notification for the user
-							console.log("No lat/long");
-						}
-						else {
-							var lat = Number(eventsResponse._embedded.events[i]._embedded.venues[0].location.latitude);
-							var long = Number(eventsResponse._embedded.events[i]._embedded.venues[0].location.longitude);
+			// grab venue name from each event
+			venueName = eventsResponse._embedded.events[i]._embedded.venues[0].name;
+			// console.log("venue " + (i+1) + " name: " + venueName);
+			
+			// grab concert time
+			// validate that the time has been set first
+			if (eventsResponse._embedded.events[i].dates.start.timeTBA == false && eventsResponse._embedded.events[i].dates.start.noSpecificTime == false) {
+				time = eventsResponse._embedded.events[i].dates.start.localTime;
+				formatTime = moment(time, "HH:mm:ss").format("h:mm A");
+			}
+			else if (eventsResponse._embedded.events[i].dates.start.timeTBA == false && eventsResponse._embedded.events[i].dates.start.noSpecificTime == true) {
+				formatTime = "This event has no specific time.";
+			}
+			else {
+				formatTime = "This event's time hasn't been set yet!";
+			}
 
-								// console.log(lat);
-								// console.log(long);
+			// grab concert start date
+			// validate that the date has been set first
+			if (eventsResponse._embedded.events[i].dates.start.dateTBA == false && eventsResponse._embedded.events[i].dates.start.dateTBD == false){
+				startDate = eventsResponse._embedded.events[i].dates.start.localDate;
+				// console.log("date: " + startDate);
+			}
+			else {
+				startDate = "The date hasn't been set yet!";
+			}
 
-							var marker = new google.maps.Marker({
-								position: {lat: lat, lng: long},
-								map: map
-							});
+			// grab ticket link
+			ticketLink = eventsResponse._embedded.events[i].url;
+			// console.log("Tickets for Concert " + (i+1) + ": " + ticketLink);
+			
+			$('#concerts-display').append(
+				"<div class='panel panel-info searchContent'>" +
+				"<div class='panel-heading' id='searchResults'>" +
+				"<h3 class='panel-title searchResultsTitle'>" +
+				concertName + " <small>" + city + ", "  + state + 
+				"</small>" + "</h3></div>" +
+				"<div class='panel-body searchResultsContent'>" +
+				venueName + "<br/>" + formatTime + "<br/>" +
+				"<a href='" + ticketLink + "'>Purchase Tickets Here</a>" +
+				"</div></div>"
+			);
+		}
+	}
+
+	//ARTIST INFORMATION
+	function ticketmasterRequest(artist){
+		console.log("Artist: " + artist);
+
+		// This will be the URL to obtain the the unique attractionId for the artist 
+		// We need this so we can find the upcoming events for the exact artist
+		var attractionsURL = "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=JuO6exfNpASFTwptmBPvKKqdGYrP7S96&keyword=" + artist;  
+
+		// AJAX REQUEST #1: Obtaining information about artist
+		$.ajax({
+			url: attractionsURL,
+			method: "GET"
+		})
+		.done(function(artistResponse){
+			console.log(artistResponse);
+
+			var checkArtist = artistResponse.page.totalElements;
+
+			// If input error is incorrect, alert the user. Otherwise, continue...
+			if (checkArtist === 0) {
+				// This is the message for the user, uncomment when .search-error is added to html
+				$(".search-error").html("*Sorry, try again");
+				console.log("Sorry, try again");
+			}
+			else {
+				var artistId = artistResponse._embedded.attractions[0].id; 
+
+				var eventsURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=JuO6exfNpASFTwptmBPvKKqdGYrP7S96&attractionId="+ artistId +  "&countryCode=" + countryCode + "&sort=" + dateOrder + "&endDateTime=" + sixMonthsLimit + "Z";
+					
+				// AJAX REQUEST #2: Obtaining information about artist's upcoming events
+				$.ajax({
+					url: eventsURL,
+					method: "GET"
+				}) 
+				.done(function(eventsResponse){
+					console.log(eventsResponse);
+
+					var upcomingEvents = eventsResponse.page.totalElements;
+
+					// If there are no concerts, alert the user. Otherwise, continue...
+					if (upcomingEvents === 0) {
+						// Display this message to the user if there are no shows
+						$("#concerts-display").html("<h4>Sorry, it seems there are no upcoming shows. Please try again at a later time or search for another artist/band.</h4>");
+
+						console.log("It seems there are no upcoming shows");
+					}
+					else {
+						console.log(eventsResponse);
+
+						concertInformation(eventsResponse);
+
+						// Google Maps Markers
+						var numEvents = eventsResponse._embedded.events;
+						for (var i = 0; i < numEvents.length ; i++) {
+							// Sometimes there is not lat/long for venues, so if-else statement
+							if(eventsResponse._embedded.events[i]._embedded.venues[0].location === undefined){
+								// We need to change this console.log to some notification for the user
+								console.log("No lat/long");
+							}
+							else {
+								var lat = Number(eventsResponse._embedded.events[i]._embedded.venues[0].location.latitude);
+								var long = Number(eventsResponse._embedded.events[i]._embedded.venues[0].location.longitude);
+
+									console.log("lat: " + lat);
+									console.log("long: " + long);
+
+								var marker = new google.maps.Marker({
+									position: {lat: lat, lng: long},
+									map: map
+								});
+							}
 						}
 					}
-				}
-			});
-		}
+				});
+			
+				// Grabbing the artist's twitter url and calling the twitter function
+				var twitterURL = artistResponse._embedded.attractions[0].externalLinks.twitter[0].url;
+				// console.log(twitterURL);
+				twitter(twitterURL);
+
+				var youtubeURL = artistResponse._embedded.attractions[0].externalLinks.youtube[0].url;
+				youtube(youtubeURL);
+
+			}
+		});
+	}
+
+	// MAIN PROCESSES ============================================
+
+	var artist = "";
+
+	$("#search").on("click", function(event){ 
+		event.preventDefault();
+
+		$('#bottom').show();
+		initMap();
+
+		// This is the user's input
+		artist = $("#artist-input").val().trim();
+
+		$("#artist-input").val("");
+		$("#twitcontainer").empty();
+		$("#concerts-display").empty();
+
+		console.log("Artist: " + artist);
+
+		ticketmasterRequest(artist);
+
 	});
-}
 
-// MAIN PROCESSES ============================================
+	$('#search2').on("click", function(event){ 
+		event.preventDefault();
 
-$("#search").on("click", function(event){ 
-	event.preventDefault();
+		$('#bottom').show();
+		initMap();
 
-	// This is the user's input
-	var artist = $("#artist-input").val().trim();
+		// This is the user's input
+		artist = $("#artist-input2").val().trim();
 
-	$("#artist-input").val("");
-	$("#twitcontainer").empty();
-	$("#concerts-display").empty();
+		$("#artist-input2").val("");
+		$("#twitcontainer").empty();
+		$("#concerts-display").empty();
 
-	ticketmasterRequest(artist);
+		console.log("Artist: " + artist);
+
+		ticketmasterRequest(artist);
+
+	});
 
 });
-
-});
-
-
-
-
 /*
 * panel content (grab from under events ajax call)
 * concert name
@@ -220,7 +273,6 @@ $("#search").on("click", function(event){
 * address
 * venue link
 
-put in for loop & grab first 10 or limit response to 10
 
 Dates
 * objects -> _embedded -> events array & index number -> dates -> start -> grab local date and local time from here
